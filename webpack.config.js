b8575a6b543fbcca43cd1d939bdc3c81f5c46c80
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-05 17:37:25 -0500

    Created initial config

--- webpack.config.js
+++ webpack.config.js
@@ -4,3 +4,13 @@
  * Date Created: December 4, 2022
  *  Description: Webpack configuration file
 ******************************************************************************/
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
