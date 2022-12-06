98b0157b2822db40b873efd01318449e9e2a4195
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-05 19:16:17 -0500

    Set things up so there is a class with a background color.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@
  * Date Created: December 4, 2022
  *  Description: Functions that support implementation of Restaurant website
 ******************************************************************************/
+import _ from 'lodash';
+import './styles.css';
+console.log("hi hi");
+
+const container = document.querySelector('#content');
+
+function component() {
+    const element = document.createElement('div');
+    element.classList.add('main');
+    
+    return element;
+}
+
+container.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,15 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 };
