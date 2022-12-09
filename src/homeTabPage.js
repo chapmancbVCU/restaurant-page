43278ebe87e9ced08644a880d33260fb855820a3
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-08 19:36:36 -0500

    build and deploy

--- src/homeTabPage.js
+++ src/homeTabPage.js
@@ -21,11 +21,60 @@ export class Home extends Page {
         mainContent.appendChild(pageTitle);
 
         const aboutUs = document.createElement('p');
+        aboutUs.classList.add('page-section-paragraph');
         aboutUs.textContent = 'At Burnt Popcorn Eatery we offer different types of popcorn but our specialty is burnt popcorn.  We also offer several types of delicious beverages and smoothies that are perfect for washing down our delicious snacks.';
         mainContent.appendChild(aboutUs);
 
+        // Describe hours
         const hoursHeader = document.createElement('h3');
         hoursHeader.textContent = 'Hours';
         mainContent.appendChild(hoursHeader);
+
+        const hoursContainer = document.createElement('div');
+        hoursContainer.classList.add('hours-container');
+        Home.hours(hoursContainer);
+
+
+
+
+
+        mainContent.appendChild(hoursContainer);
+    }
+
+    static hours(parentContainer) {
+        const sunday = document.createElement('p');
+        sunday.textContent = 'Sunday: Closed';
+        sunday.classList.add('daily-hours');
+        parentContainer.appendChild(sunday);
+
+        const monday = document.createElement('p');
+        monday.textContent = 'Monday: 3PM - 10PM';
+        monday.classList.add('daily-hours');
+        parentContainer.appendChild(monday);
+
+        const tuesday = document.createElement('p');
+        tuesday.textContent = 'Tuesday: 3PM - 10PM';
+        tuesday.classList.add('daily-hours');
+        parentContainer.appendChild(tuesday);
+
+        const wednesday = document.createElement('p');
+        wednesday.textContent = 'Wednesday: 3PM - 10PM';
+        wednesday.classList.add('daily-hours');
+        parentContainer.appendChild(wednesday);
+
+        const thursday = document.createElement('p');
+        thursday.textContent = 'Thursday: 3PM - 10PM';
+        thursday.classList.add('daily-hours');
+        parentContainer.appendChild(thursday);
+
+        const friday = document.createElement('p');
+        friday.textContent = 'Friday: 3PM - 11PM';
+        friday.classList.add('daily-hours');
+        parentContainer.appendChild(friday);
+
+        const saturday = document.createElement('p');
+        saturday.textContent = 'Saturday: 11PA - 10PM';
+        saturday.classList.add('daily-hours');
+        parentContainer.appendChild(saturday);
     }
 }
