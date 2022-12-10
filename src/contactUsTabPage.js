074aede5d5505b52d69164e66cc027ccde3a77c9
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-10 16:48:43 -0500

    Begin work on contact us page.  We have intro in contact info.

--- src/contactUsTabPage.js
+++ src/contactUsTabPage.js
@@ -36,5 +36,48 @@ export class ContactUs extends Page {
         const pageTitle = document.createElement('h2');
         pageTitle.textContent = 'Contact Us';
         mainContent.appendChild(pageTitle);
+
+        // Contact us intro
+        ContactUs.contactIntro(mainContent);
+
+        // Contact info
+        ContactUs.contactInfo(mainContent);
+    }
+
+    static contactInfo(parentContainer) {
+        const contactInfo = document.createElement('h3');
+        contactInfo.textContent = 'How To Reach Us';
+        parentContainer.appendChild(contactInfo);
+
+        const contactInfoContainer = document.createElement('div');
+        contactInfoContainer.classList.add('paragraph-container');
+
+        const phoneNumber = document.createElement('p');
+        phoneNumber.textContent = 'Call Us: 123-456-7890';
+        contactInfoContainer.appendChild(phoneNumber);
+
+        const email = document.createElement('p');
+        email.textContent = 'E-mail: burntpopcorn@fakedomain.com';
+        contactInfoContainer.appendChild(email);
+
+        parentContainer.appendChild(contactInfoContainer);
+    }
+
+    static contactIntro(parentContainer) {
+        const contactUsContainer = document.createElement('div');
+        contactUsContainer.classList.add('paragraph-container');
+
+        const reachUs = document.createElement('p');
+        reachUs.classList.add('contact-us');
+        reachUs.textContent = "We value every customer's feedback.  Whether it be complements or otherwise, you can reach us through many different methods including social media or in person at our location in downtown Westchestertonfieldville.";
+        contactUsContainer.appendChild(reachUs);
+
+        const catering = document.createElement('p');
+        catering.classList.add('contact-us');
+        catering.textContent = "We also offer catering services and our food truck operates daily at the local college campus.";
+        contactUsContainer.appendChild(catering);
+
+        parentContainer.appendChild(contactUsContainer);
+
     }
 }
